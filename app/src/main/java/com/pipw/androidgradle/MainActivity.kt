f87ae40cc2cdb79903cbcff1d3b0b3407f4d2fc3
zhaenpg <446432914@qq.com>
2022-12-07 23:34:04 +0800

    protobuf使用示例

--- app/src/main/java/com/pipw/androidgradle/MainActivity.kt
+++ app/src/main/java/com/pipw/androidgradle/MainActivity.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.demowork1.PersonProto
 
 private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +20,24 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "onCreate: button1 click")
         }
 
-        findViewById<Button>(R.id.btn2).setOnClickListener(object :View.OnClickListener{
+        findViewById<Button>(R.id.btn2).setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 Log.d(TAG, "onClick: button2 click")
             }
         })
+
+        //protobuf的使用示例
+        val person = PersonProto.Person.newBuilder().setName("Tom")
+            .setId(11)
+            .setBoo(false)
+            .setEmail("ssss")
+            .setPhone("1245")
+            .build()
+
+        val data = PersonProto.Person.parseFrom(person.toByteArray())
     }
 
-    fun count(){
+    fun count() {
         Thread.sleep(10L)
     }
 
